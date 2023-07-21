776df9394c090646c67672a2021df5a4dd30c825
Justin Rinehuls <jrinehuls@gmail.com>
2023-07-20 23:13:54 -0400

    Add handler methods

--- src/main/java/com/jrinehuls/gradesubmission/controller/StudentController.java
+++ src/main/java/com/jrinehuls/gradesubmission/controller/StudentController.java
@@ -1,11 +1,35 @@
 package com.jrinehuls.gradesubmission.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jrinehuls.gradesubmission.entity.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable Long id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Student>> getStudents() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student) {
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable Long id) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
